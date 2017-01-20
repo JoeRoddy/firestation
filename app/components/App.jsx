fed059a43fb3a560c0774957aaa98c4de43812d7
drsloth <bennysully@gmail.com>
2017-01-20 08:45:46 -0500

    Updates * Add css-loader and electron-remote modules * Add app.global.css with css reset * Import app.global.css in index.js * Use remote in App.jsx to set width and height to dimensions of current window * Add ridiculous hello world marquee set in App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+const remote = require('electron').remote;
 
 @observer
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
+    const { height, width } = remote.getCurrentWindow().getBounds();
     return (
-      <div>Hello World</div>
+      <div style={{ height: height - 25, width: width, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <marquee><pre>Hello World!</pre></marquee>
+        <marquee><pre>    Hello World!</pre></marquee>
+        <marquee><pre>        Hello World!</pre></marquee>
+        <marquee><pre>            Hello World!</pre></marquee>
+        <marquee><pre>                Hello World!</pre></marquee>
+        <marquee><pre>                    Hello World!</pre></marquee>
+        <marquee><pre>                        Hello World!</pre></marquee>
+        <marquee><pre>                            Hello World!</pre></marquee>
+        <marquee><pre>                                Hello World!</pre></marquee>
+        <marquee><pre>                                    Hello World!</pre></marquee>
+        <marquee><pre>                                        Hello World!</pre></marquee>
+        <marquee><pre>                                            Hello World!</pre></marquee>
+        <marquee><pre>                                                Hello World!</pre></marquee>
+        <marquee><pre>                                                    Hello World!</pre></marquee>
+        <marquee><pre>                                                        Hello World!</pre></marquee>
+        <marquee><pre>                                                            Hello World!</pre></marquee>
+        <marquee><pre>                                                                Hello World!</pre></marquee>
+        <marquee><pre>                                                                    Hello World!</pre></marquee>
+        <marquee><pre>                                                                        Hello World!</pre></marquee>
+        <marquee><pre>                                                                            Hello World!</pre></marquee>
+      </div>
     );
   }
 }
